8f0673365c7ea28038277871c207e97f5d63652e
Carl Thomason <74626366+carlthomason@users.noreply.github.com>
2021-04-02 08:43:28 -0500

    Updates

---
+++ precache-manifest.994abcac2bfd22f18649fe01716b8fe1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12c81a043d2758ee4ebe225cab4e8cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d8722cf71175681c532",
+    "url": "/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "938f2e40988e68c0b92b",
+    "url": "/static/css/main.0e364d06.chunk.css"
+  },
+  {
+    "revision": "1d8722cf71175681c532",
+    "url": "/static/js/2.060e9d8a.chunk.js"
+  },
+  {
+    "revision": "215e3fee2ac952a6c791784377015123",
+    "url": "/static/js/2.060e9d8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "938f2e40988e68c0b92b",
+    "url": "/static/js/main.db8fab3a.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "df1f0ba3edd50533dd221b5502fd7fd1",
+    "url": "/static/media/Backyard_beer.df1f0ba3.png"
+  },
+  {
+    "revision": "5bcabf4b3208cb92c837abd71bbe6bbc",
+    "url": "/static/media/outdoors.5bcabf4b.png"
+  },
+  {
+    "revision": "8b8a164feeadc023c5e99a9984176d49",
+    "url": "/static/media/port.8b8a164f.png"
+  },
+  {
+    "revision": "d524261d9e420caa90b6b7f8b720cc9e",
+    "url": "/static/media/thomason.d524261d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.264c55c88d2efda8ddb54848a20b0f68.js"
+  "/precache-manifest.994abcac2bfd22f18649fe01716b8fe1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
